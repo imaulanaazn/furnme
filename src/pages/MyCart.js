182f7b9b3fca3f7ec480d256cabb4b27f11b9c8a
Irham Maulana <i.maulana.azn@gmail.com>
2023-01-08 09:20:33 +0700

    returning the react eject

--- src/pages/MyCart.js
+++ src/pages/MyCart.js
@@ -1,34 +1,12 @@
 import '../styles/cart.css';
 import { useState } from 'react';
-import jwt from 'jsonwebtoken';
-import { useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
 import MyCartNavbar from '../components/MyCartNavbar';
 import MyCartMain from '../components/MyCartMain';
 import MyCartAside from '../components/MyCartAside';
 
-const verifyToken = (token) => {
-  try {
-    const decoded = jwt.verify(token, 'my-secret-key');
-    return decoded;
-  } catch (error) {
-    return null;
-  }
-};
-
 export default function MyCart() {
-  const navigate = useNavigate();
   const [totalCost, setTotalCost] = useState(0);
 
-  const token = Cookies.get('token');
-  if (!token) {
-    navigate('/');
-  } else {
-    const decoded = verifyToken(token);
-    if (!decoded) {
-      navigate('/');
-    }
-  }
   return (
     <>
       <MyCartNavbar />
