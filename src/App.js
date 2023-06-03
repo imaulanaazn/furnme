aef7f3fa1eacd60bc1167b6256e727ed2310aae6
Irham Maulana <i.maulana.azn@gmail.com>
2023-06-03 16:38:16 +0700

    delete forsale page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 import Shop from './pages/Shop';
 import Home from './pages/Home';
 import About from './pages/About';
-import Forsale from './pages/Forsale';
 import Auth from './pages/Auth';
 import MyCart from './pages/MyCart';
 import ProductDetail from './pages/ProductDetail';
@@ -19,7 +18,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/shop" element={<Shop />} />
-        <Route path="/forsale" element={<Forsale />} />
         <Route path="/auth" element={isLogin ? <Navigate to="/" /> : <Auth />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/success" element={<Success />} />
