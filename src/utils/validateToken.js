348775adacaa0f60e694e0f2cb1b23cb0ada3fa7
Irham Maulana <i.maulana.azn@gmail.com>
2023-07-24 13:50:12 +0700

    refactor validateToken function

--- src/utils/validateToken.js
+++ src/utils/validateToken.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const ROOT_API = process.env.REACT_APP_PUBLIC_API;
-export default async function validateToken(token) {
-  const axiosInstance = axios.create({
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
 
-  const result = await axiosInstance.post(`${ROOT_API}/validate`)
-
-    .then((res) => res)
-    .catch((err) => err);
-
-  return result;
+export default async function validateToken() {
+  const token = Cookies.get('token');
+  const decodedToken = atob(token);
+  console.log(decodedToken);
+  try {
+    const result = await axios.post(`${ROOT_API}/users/validate`, {}, {
+      headers: {
+        Authorization: `Bearer ${decodedToken}`,
+      },
+    });
+    return result.data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
