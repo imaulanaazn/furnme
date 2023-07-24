e1f497276c6464883e93a96fae64bf108a85592d
Irham Maulana <i.maulana.azn@gmail.com>
2023-07-24 14:57:34 +0700

    delete AllProductsNavbar and cartSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './slices/cartSlice';
 import authReducer from './slices/auth';
 
 export default configureStore({
   reducer: {
-    cart: cartReducer,
     auth: authReducer,
   },
 });
