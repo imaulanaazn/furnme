c868f338ecffab889ef231e5ac2b5a73145f8144
Irham Maulana <i.maulana.azn@gmail.com>
2023-08-01 05:42:25 +0700

    redesign ProductDetail component and integrate it with api

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,114 +1,144 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
 import Navbar from '../components/Navbar';
-import validateToken from '../utils/validateToken';
+import { getProductById } from '../utils/fetchData';
+
+const productInitialData = {
+  category: [{
+    name: '',
+    thumbnail: '',
+    _id: '',
+  }],
+  comments: [],
+  description: '',
+  discount: 0,
+  flashSaleEnd: null,
+  flashSaleAtart: null,
+  images: [],
+  name: '',
+  price: 0,
+  rating: 0,
+  similarProduct: [],
+  _id: '',
+};
 
 export default function ProductDetail() {
-  const ROOT_URL = process.env.REACT_APP_PUBLIC_API;
-  const [furniture, setFurniture] = useState(null);
+  const [product, setProduct] = useState(productInitialData);
   const { id: productId } = useParams();
-  const [prodQuantity, setProdQuantity] = useState('');
-  const navigate = useNavigate();
+  const [prodQuantity, setProdQuantity] = useState(1);
+  // const navigate = useNavigate();
 
   useEffect(() => {
-    async function getProductData() {
-      const { data } = await axios(`${ROOT_URL}/product/${productId}`);
-      setFurniture(data);
-    }
-    getProductData();
-  }, [productId]);
-
-  async function getUserId() {
-    const token = Cookies.get('token');
-    const decodedToken = atob(token);
-    const data = await validateToken(decodedToken);
-    const userId = data?.data?.user?.id;
-    return userId;
-  }
-
-  async function submitCartData(e) {
-    e.preventDefault();
-    const userId = await getUserId()
-      .then((res) => res)
-      .catch((err) => err);
-    if (userId) {
-      const cartData = {
-        userId,
-        products: {
-          productId,
-          quantity: prodQuantity,
-        },
-      };
-      await axios.post(`${ROOT_URL}/cart`, cartData)
+    async function callAPI() {
+      await getProductById(productId)
         .then((res) => {
-          if (res.status === 200) {
-            alert('product added');
-          } else {
-            alert('error happend, try again later');
-          }
+          setProduct(res.data ? res.data : {});
         })
         .catch((err) => console.log(err));
-      setProdQuantity('');
-    } else {
-      navigate('/auth');
     }
-  }
+
+    callAPI();
+  }, []);
+
+  console.log(product);
 
   return (
     <>
       <Navbar position="static" />
 
-      <section className="w-11/2 md:h-screen md:py-20
-       md:px-10 lg:px-20 py-12 m-auto flex items-center"
-      >
-        <div className="w-full flex flex-col md:flex-row md:justify-between items-center lg:gap-20">
-          <div className="relative lg:w-2/5">
-            <img id="image" className="w-full h-full" src={furniture?.img} alt="" />
-            <div className="arrows w-full absolute inset-y-1/2 flex justify-between px-3">
-              <button type="button" id="prev"><i className="fa-solid fa-chevron-left" /></button>
-              <button type="button" id="next"><i className="fa-solid fa-chevron-right" /></button>
-            </div>
-          </div>
-          <div className="p-5 lg:w-3/5">
-            <h4 className="text-base font-thin mb-10">{furniture?.categories[0]}</h4>
-            <h1 className="text-5xl font-medium">{furniture?.title}</h1>
-            <h2 className="text-3xl font-normal mt-2">
-              $
-              {furniture?.price}
-            </h2>
-            <p className="text-base text-slate-700 my-10 max-w-md">
-              {furniture?.desc}
-            </p>
-            <form className="flex items-center md:justify-start justify-between space-x-5 my-7" onSubmit={(e) => { submitCartData(e); }}>
-              <input
-                className="w-24 h-8 px-4 h-10 border border-slate-600 outline-0"
-                type="number"
-                id="amount"
-                required
-                value={prodQuantity}
-                onChange={(e) => { setProdQuantity(e.target.value); }}
-              />
-              <button
-                type="submit"
-                className="flex items-center h-10 px-4 border border-orange-200
-                 bg-orange-200 hover:bg-orange-300 hover:border hover:border-gray-600"
-              >
-                <i className="fa-solid fa-cart-shopping text-xl mr-3" />
-                <span>Add To Cart</span>
-              </button>
-            </form>
-            <button
-              type="button"
-              className="flex items-center justify-center border border-rose-400
-               h-10 w-10 rounded-md hover:bg-rose-400 hover:text-white"
-            >
-              <i className="fa-regular fa-heart text-xl" />
+      <section className="product-wrapper w-full xl:px-20 lg:px-12 md:px-16 sm:px-10 px-6 pt-36 md:pt-44 pb-8 md:pb-12 flex flex-col lg:flex-row md:justify-between xl:gap-20 lg:gap-14 gap-7">
+        <div className="product-left relative lg:w-1/2 h-min">
+          <img id="image" className="w-full lg:w-11/12 xl:w-10/12 h-auto mx-auto" src={product.images[0]} alt="" />
+          <div className="arrows inset-y-1/2 w-full xl:w-10/12 absolute flex justify-between px-3 xl:inset-x-1/2 xl:-translate-x-1/2">
+            <button type="button" id="prev" className="w-8 h-8 md:w-10 md:h-8 lg:w-8 lg:h-8 bg-white rounded-full opacity-80 hover:opacity-100 -translate-y-1/2 transition-all">
+              <i className="fa-solid fa-chevron-left" />
+            </button>
+            <button type="button" id="next" className="w-8 h-8 md:w-10 md:h-8 lg:w-8 lg:h-8 bg-white rounded-full opacity-80 hover:opacity-100 -translate-y-1/2 transition-all">
+              <i className="fa-solid fa-chevron-right" />
             </button>
           </div>
+        </div>
+
+        <div className="product-right lg:w-1/2">
+          <div className="product-categories mb-4">
+            {
+            product.category.map((category) => (
+              <Link to={`/shop?category=${category._id}`} className="product-category font-light md:text-xl lg:text-sm text-slate-600 border border-slate-600 rounded-md px-2 py-1 max-w-max mr-2">
+                {product.category[0].name}
+              </Link>
+            ))
+          }
+          </div>
+          <h1 className="product-name text-4xl md:text-5xl lg:text-4xl">{product.name}</h1>
+          <h2 className="product-price text-lg md:text-2xl lg:text-lg font-normal my-1 md:my-2 lg:my-1">
+            $
+            {product.price}
+          </h2>
+          <div className="product-info flex items-end mt-1">
+            <p className="product-info__discount text-sm md:text-xl lg:text-sm bg-rose-500 text-white py-1 px-1 rounded">
+              {product.discount ? product.discount : ''}
+              {' '}
+              %
+            </p>
+            <span className="product-info__discounted-price text-sm md:text-base lg:text-sm text-slate-600 font-light ml-2 mr-4 md:ml-3 md:mr-5 lg:ml-2 lg:mr-4">
+              <del>
+                {product.discount ? `$${product.price - ((product.price * product.discount) / 100)}` : ' '}
+              </del>
+            </span>
+            <div className="product-info__rating">
+              {
+                 Array.from({ length: Math.round(product.rating) }, (_, i) => i + 1)
+                   .map(() => (
+                     <i className="fa-solid fa-star text-sm sm:text-lg lg:text-sm text-yellow-500" />
+                   ))
+              }
+            </div>
+          </div>
+          <p className="product-desc my-6 md:my-8 lg:my-6 md:text-lg lg:text-base text-slate-700 max-w-md">
+            {product.description}
+          </p>
+          <button type="button" className="product-like flex items-center justify-center">
+            <i className="fa-solid fa-heart text-2xl md:text-3xl lg:text-2xl text-slate-300 hover:text-rose-500 transition-all" />
+          </button>
+          <form className="product-quantity my-4 md:my-5 lg:my-4 w-max" onSubmit={() => { }}>
+            <div className="product-quantity__counter flex space-x-2 mb-4 md:mb-5 lg:mb-4">
+              <button
+                type="button"
+                disabled={prodQuantity <= 1}
+                className="product-quantity__dec w-9 h-9 md:w-10 md:h-10 lg:w-9 md:h-9 md:text-lg border border-slate-700 rounded-md hover:bg-orange-200 hover:border-none transition-all"
+                onClick={() => setProdQuantity(prodQuantity - 1)}
+              >
+                -
+              </button>
+              <input
+                className="product-quantity__value md:text-lg px-4 border border-slate-600 outline-0 h-9 w-20 md:h-10 md:w-24 md:h-9 rounded-md"
+                type="number"
+                id="amount"
+                max={1000}
+                required
+                value={prodQuantity}
+                onChange={(e) => { setProdQuantity(parseInt(e.target.value, 10)); }}
+              />
+              <button
+                type="button"
+                className="product-quantity__inc md:text-lg w-9 h-9 md:w-10 md:h-10 lg:w-9 md:h-9 border border-slate-700 rounded-md hover:bg-orange-200 hover:border-none transition-all"
+                onClick={() => setProdQuantity(prodQuantity + 1)}
+              >
+                +
+              </button>
+            </div>
+            <button
+              type="submit"
+              className="product-cart__add flex items-center h-10 w-full md:h-11 px-4 border border-orange-200
+                 bg-orange-200 hover:bg-orange-300 rounded-md transition-all"
+            >
+              <i className="fa-solid fa-cart-shopping text-xl mr-3 text-orange-800" />
+              <span>Add To Cart</span>
+            </button>
+          </form>
+
         </div>
       </section>
     </>
