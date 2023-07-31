e8e37853ef67418e92c67cd99e1a31c4337f9587
Irham Maulana <i.maulana.azn@gmail.com>
2023-08-01 05:41:14 +0700

    refactor getUserData utils

--- src/utils/getUserData.js
+++ src/utils/getUserData.js
@@ -3,7 +3,9 @@ import jwtDecode from 'jwt-decode';
 
 export default function getUserData() {
   const token = Cookies.get('token');
-  const decodedToken = atob(token);
-  const userData = jwtDecode(decodedToken);
-  return userData;
+  const decodedToken = token ? atob(token) : '';
+  if (decodedToken) {
+    return jwtDecode(decodedToken);
+  }
+  return {};
 }
