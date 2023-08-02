ce17adb27463473a81581b5643cf1d5bb3a9898e
Irham Maulana <i.maulana.azn@gmail.com>
2023-08-02 15:43:20 +0700

    only show recenlty viewed section when user loggedin

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,5 @@
 import '../styles/shop.css';
+import { useSelector } from 'react-redux';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 // import TopRated from '../components/TopRated';
@@ -8,13 +9,18 @@ import RecentlyViewed from '../components/RecentlyViewed';
 import NewArrival from '../components/NewArrival';
 
 export default function Shop() {
+  const { isLogin } = useSelector((state) => state.auth);
   return (
     <>
       <Navbar bg="bg-white" />
       <Recommendation />
       <NewArrival />
       <Products />
-      <RecentlyViewed />
+      {
+        isLogin
+          ? <RecentlyViewed />
+          : ''
+      }
       <Footer />
     </>
   );
